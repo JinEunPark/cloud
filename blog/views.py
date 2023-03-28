c1f3203f034da8238c8e22ed0e0d666723fb6f13
JinEunPark <wlsdmsrnfl@naver.com>
2023-03-28 15:37:59 +0900

    CBD 변경

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
-def index(request):# 첫번째 파라미터는 항상 reqeust
-    posts = Post.objects.all().order_by("created_at")
+# def index(request):# 첫번째 파라미터는 항상 reqeust
+#     posts = Post.objects.all().order_by("created_at")
+#
+#     return render(
+#         request, #고정 암기할래그
+#         'blog/post_list.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+class PostList(ListView): #목록 보기화면 완성
+    model = Post
 
-    return render(
-        request, #고정 암기할래그
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
-def single_post_page(request, post_num):
-    post = Post.objects.get(pk=post_num)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post
-        }
-    )
+
+# def single_post_page(request, post_num):
+#     post = Post.objects.get(pk=post_num)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post':post
+#         }
+#     )
+
+class PostDetail(DetailView):
+    model = Post
