3d94f6d947427d10a7590c207e94662630dd6a1d
JinEunPark <wlsdmsrnfl@naver.com>
2023-04-04 16:16:24 +0900

    category를 post에 추가함.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 admin.site.register(Post)##DB로 만든 post class 를 가져옴
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)} ##slug 필드 안에 카테고리 이름이 자동으로 삽입될 수있게 만들었음. 안녕하세요 박진은 입니다 잘부탇드립니다
+
+admin.site.register(Category,CategoryAdmin)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,20 @@ import os.path
 
 from django.contrib.auth.models import User
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)## 카테고리 이름
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)## 읽을 수 잇는 텍스트로 url을 만들고 싶을 때 사용함.
+    #유니코드를 사용가능할 수 있게 설정함.
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories' #change verbose name not default
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
@@ -17,6 +31,9 @@ class Post(models.Model):
 
     author = models.ForeignKey(User,  null=True, on_delete=models.CASCADE) #장고의 기본 유저를 추가함 필수로 들어감
     # CASCAD 를 사용해서 유저가 삭제되면 글도 삭제됨
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    ##유효성 검사할 때 공란을 혀용함 그리고 DB상에 공란으로 저장될 수 있도록 허용함.
 
     def get_absolute_url(self):
         return  f'/blog/{self.pk}'
